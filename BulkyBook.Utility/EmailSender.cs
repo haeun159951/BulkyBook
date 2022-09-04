6f7dbd40bed2b13560da44ac2b227c7d7581ccec
HaeunKim <haeun159951@gmail.com>
2022-09-03 22:20:27 -0400

    first commit

---
+++ BulkyBook.Utility/EmailSender.cs
@@ -0,0 +1,45 @@
+﻿
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MailKit.Net.Smtp;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        public string SendGridSecret { get; set; }
+
+        public EmailSender(IConfiguration _config)
+        {
+            SendGridSecret = _config.GetValue<string>("SendGrid:SecretKey");
+        }
+
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            //var emailToSend = new MimeMessage();
+            //emailToSend.From.Add(MailboxAddress.Parse("hello@dotnetmastery.com"));
+            //emailToSend.To.Add(MailboxAddress.Parse(email));
+            //emailToSend.Subject = subject;
+            //emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = htmlMessage };
+
+            //send email
+            //using (var emailClient = new SmtpClient())
+            //{
+            //    emailClient.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+            //    emailClient.Authenticate("dotnetmastery@gmail.com", "DotNet123$");
+            //    emailClient.Send(emailToSend);
+            //    emailClient.Disconnect(true);
+            //}
+
+            return Task.CompletedTask;
+
+
+        }
+    }
+}
